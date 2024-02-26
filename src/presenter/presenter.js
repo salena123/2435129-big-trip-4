4b0bf63e25475a01837f766ddb5e88e4168e63bc
алена <sasmurka@mail.ru>
2024-02-26 10:28:01 +0500

    Время разбивать камни

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -2,6 +2,7 @@ import FormOfCreationView from '../view/form-of-creation-view.js';
 import PointView from '../view/point-view.js';
 import SortingView from '../view/sorting-view.js';
 import { render, RenderPosition } from '../render.js';
+const CONSTANT_NUMBER = 3;
 
 export default class BordPresenter {
   sortingComponent = new SortingView();
@@ -15,7 +16,7 @@ export default class BordPresenter {
     render(this.sortingComponent, this.container, RenderPosition.AFTERBEGIN);
     render(this.formOfCreationComponent, this.container);
 
-    for(let i = 0; i < 3; i++) {
+    for(let i = 0; i < CONSTANT_NUMBER; i++) {
       render(new PointView(), this.formOfCreationComponent.getElement());
     }
   }
